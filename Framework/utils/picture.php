4e746efbce48515ba0e8d16e1359c16c08dc28f0
VitaliyMorozov <v.morozov@interesnee.ru>
2018-05-29 13:39:19 +0600

    update the class picture

--- Framework/utils/picture.php
+++ Framework/utils/picture.php
@@ -399,8 +399,6 @@ class Picture
    */
   protected function getRightSize($mode, $dstWidth, $dstHeight, $srcWidth, $srcHeight, $maxWidth = 0, $maxHeight = 0)
   {
-    $dstWidth = abs((int) filter_var($dstWidth, FILTER_SANITIZE_NUMBER_INT));
-    $dstHeight= abs((int) filter_var($dstHeight, FILTER_SANITIZE_NUMBER_INT));
     switch ($mode)
     {
       case self::PIC_MANUAL; 
